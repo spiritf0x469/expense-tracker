bb2cd0a0d439a0fe07ef5df5967c72fef60e8358
sameer sainii <justanid2006@gmail.com>
2026-06-27 15:37:26 +0530

    fixes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const api=axios.create({
-    baseURL:"https://expense-tracker-api-vpql.onrender.com/api"
+    baseURL:import.meta.env.VITE_API_URL
 })
 api.interceptors.request.use((config)=>{
     const token=localStorage.getItem("token")
